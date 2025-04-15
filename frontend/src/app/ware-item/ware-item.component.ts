cc183824dcdab89a502db4197d390687f8e07775
Nis Studie <bruger@MacBook-Pro-tilhrende-Bruger.local>
2025-04-15 20:46:33 +0200

    simplified the wareItem component

--- frontend/src/app/ware-item/ware-item.component.ts
+++ frontend/src/app/ware-item/ware-item.component.ts
@@ -16,45 +16,23 @@ export class WareItemComponent implements OnChanges
   @Input() ware: Ware | null = null;
   barcodeNotFound: boolean | null = null;
   
-  constructor(private WareItemService : WareItemService ) {}
+  constructor(private wareItemService : WareItemService ) {}
 
-  ngOnChanges(changes : SimpleChanges) : void 
+  ngOnChanges(): void 
   {
-      const barcodeChanged = changes['barcode'];
-      const wareChanged = changes['ware'];
-
-      if(barcodeChanged)
-        this.barcode = changes['barcode'].currentValue;
-      if(wareChanged)
-        this.ware = changes['ware'].currentValue;
-
-
-      //if only the barcode is provided, get ware info
-
-      //and also get ware info if barcode has been updated 
-      //and it doesnt match our ware.barcode property
-      const shouldGetWareByBarcode = this.barcode && 
-      (!this.ware || 
-      (barcodeChanged && this.barcode !== this.ware?.barcode
-      ))
-
-
-      if(shouldGetWareByBarcode)
-      {
-        this.WareItemService.getWareByBarcode(this.barcode).subscribe({
-          next: (data) => {
-            this.ware = data; 
-            this.barcodeNotFound = false;
-          },
-          error: (err) => {
-            console.log(err);
-            if(err.status === 404) {
-              this.ware = null;
-              this.barcodeNotFound = true;
-            }
+      this.wareItemService.getWareByBarcode(this.barcode).subscribe({
+        next: (data) => {
+          this.ware = data;
+          this.barcodeNotFound = false;
+        },
+        error: (err) => {
+          console.log(err);
+          if (err.status === 404)
+          {
+            this.ware = null;
+            this.barcodeNotFound = true;
           }
-
-        });
-      }
+        }
+      });
   }
 }
